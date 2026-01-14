16e3172629c3b3fe042b737625ef537c4a7aae81
Didier Fernando Aragón Moreno <didieraragon@habi.co>
2026-01-14 12:48:07 -0500

    creación del archivo .gitignore y creacion de model para users

--- basic/users/models.py
+++ basic/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  code = models.IntegerField()
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  name = models.CharField(max_length=255)
+  profession = models.CharField(max_length=255)
+  gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female')], max_length=255)
+  city = models.CharField(max_length=255)
